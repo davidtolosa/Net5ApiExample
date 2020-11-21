b588f1914e8e839faf73a7a01ae63f4a6802d306
David Tolosa <davidtolosa5@gmail.com>
2020-11-21 10:42:28 -0300

    Add project files.

---
+++ Net5Api.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Net5Api.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ Net5Api.Infrastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Net5Api.Infrastructure
+{
+    public class Class1
+    {
+    }
+}
